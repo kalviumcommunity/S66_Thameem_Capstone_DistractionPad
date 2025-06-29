75dd9c8938f9832d553f3c20e808d626c33632cc
Thameem Ashraf <thameemashraf@Aymans-Macbook-Pro.local>
2025-06-29 22:00:48 +0530

    Created schema

--- backend/models/events.js
+++ backend/models/events.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+
 const eventSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   date: { type: Date, required: true },
-  title: { type: String, required: true },
+  title: { type: String, required: true, trim: true },
   time: { type: String, required: true }
 });
+
 module.exports = mongoose.model('Event', eventSchema);
